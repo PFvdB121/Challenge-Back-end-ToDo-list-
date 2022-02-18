1e47c8b6cb361e5f80c341fdddebe987aad3b81a
Patrique <99049609@mydavinci.nl>
2022-02-18 09:49:09 +0100

    Ik had een functie gemaakt die gebruikt wordt om een verbinding te maken met de database, en eentje om die te beveiligen, en ik had ervoor gezorgd dat die 2 functies in de file index.php gebruikt zou kunnen  worden

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php 
+	function createConnection(){
+		try {
+			$conn = new PDO("mysql:host=localhost;dbname=todo", "root", "mysql");
+			return $conn;
+		} catch (PDOException $e) {
+			print("Error!: " . $e->getMessage() . "<br>");
+		}
+	}
+?>
